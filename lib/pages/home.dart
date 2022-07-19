416e28246af1cc11bfb3fc62a89bb83d4d9d81e1
MichelleSetiyanti <mmichellesetiyanti@gmail.com>
2022-07-19 13:22:27 +0700

    60%

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import '../widgets/SummaryItem.dart';
 import 'package:http/http.dart' as http;
 import 'package:covid19/models/summary.dart';
@@ -10,7 +11,7 @@ class PagePertama extends StatelessWidget {
 
   Future getSummary() async {
     var response = await http.get(
-      Uri.parse("https://covid19.mathdro.id/api"),
+      Uri.parse("https://data.covid19.go.id/public/api/update.json"),
     );
     // print(response.body);
     var data = json.decode(response.body) as Map<String, dynamic>;
@@ -21,67 +22,74 @@ class PagePertama extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          // background(),
-          Container(
-            height: 300,
-            decoration: BoxDecoration(
-              color: Colors.indigo[900],
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+          Stack(
+            children: [
+              // background(),
+              Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[900],
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
               ),
-            ),
+              SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      header(),
+                      search(),
+                      FutureBuilder(
+                        future: getSummary(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: Text(
+                                "Laoding..",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            );
+                          }
+                          return Row(
+                            children: [
+                              SummaryItem(
+                                "CONFIRMED",
+                                "${dataSummary.confirmed.value}",
+                                Icon(Icons.person_add_alt_sharp,
+                                    color: Colors.white),
+                                Colors.amber,
+                              ),
+                              SummaryItem(
+                                "DEATHS",
+                                "${dataSummary.deaths.value}",
+                                Icon(Icons.person_off_sharp,
+                                    color: Colors.white),
+                                Color.fromARGB(255, 51, 230, 212),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          // SafeArea(
-          //   child: SingleChildScrollView(
-          //     child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //       children: [
-          //         Padding(
-          //           padding: EdgeInsets.all(16),
-          //           child: Row(
-          //             children: [
-          //               Expanded(
-          //                 child: Column(
-          //                   crossAxisAlignment: CrossAxisAlignment.start,
-          //                   children: [
-          //                     Text(
-          //                       "Hello,",
-          //                       style: TextStyle(
-          //                         color: Colors.white70,
-          //                         fontSize: 20,
-          //                       ),
-          //                     ),
-          //                     Text(
-          //                       "Michelle",
-          //                       style: TextStyle(
-          //                         color: Colors.white70,
-          //                         fontSize: 24,
-          //                         fontWeight: FontWeight.bold,
-          //                       ),
-          //                     ),
-          //                   ],
-          //                 ),
-          //               ),
-          //               Container(
-          //                 width: 50,
-          //                 height: 50,
-          //                 child: CircleAvatar(
-          //                   backgroundImage: NetworkImage(
-          //                       "https://picsum.photos/seed/picsum/200/300"),
-          //                 ),
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
+          // Container(
+          //   width: 300,
+          //   height: 300,
+          //   color: Colors.amber,
           // ),
         ],
       ),
+
       // appBar: AppBar(
       //   title: Text("data"),
       //   centerTitle: true,
@@ -103,6 +111,90 @@ class PagePertama extends StatelessWidget {
       //     }),
     );
   }
+}
+
+Widget search() {
+  return Container(
+    margin: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Row(
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Search..",
+              border: InputBorder.none,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.indigo[900],
+              ),
+              isDense: true,
+              contentPadding: EdgeInsets.all(0),
+            ),
+            textAlignVertical: TextAlignVertical.center,
+          ),
+        ),
+        Container(
+          width: 1,
+          height: 30,
+          color: Colors.indigo[900],
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.keyboard_arrow_down_rounded,
+            size: 30,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget header() {
+  return Padding(
+    padding: EdgeInsets.all(16),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                "Hello,",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Michelle",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: 50,
+          height: 50,
+          child: CircleAvatar(
+            backgroundImage:
+                NetworkImage("https://picsum.photos/200/300?random=2"),
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 Widget background() {
--- lib/widgets/SummaryItem.dart
+++ lib/widgets/SummaryItem.dart
@@ -1,44 +1,138 @@
+// import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SummaryItem extends StatelessWidget {
-  SummaryItem(this.title, this.value);
+  SummaryItem(this.title, this.value, this.logo, this.color);
   String title;
+  Icon logo;
   String value;
+  Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      margin: EdgeInsets.all(20),
-      elevation: 7,
-      child: Container(
-        height: 150,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                title,
-                style: TextStyle(color: Colors.white),
-              ),
-              Text(
-                value,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-        width: double.infinity,
-        decoration: BoxDecoration(
+    return Expanded(
+      // width: 200,
+      child: Card(
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
-          color: Colors.amber,
+        ),
+        margin: const EdgeInsets.all(11),
+        elevation: 2,
+        child: Container(
+          // height: double.infinity,
+          height: 200,
+          // width: 200,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: color,
+                      boxShadow: [
+                        BoxShadow(
+                          color: color,
+                          spreadRadius: 0,
+                          blurRadius: 4,
+                          offset: Offset(0, 1),
+                        )
+                      ]),
+                  // padding: EdgeInsets.all(20),
+                  child: logo,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  value,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
         ),
       ),
     );
+    // return Container(
+    //   decoration: BoxDecoration(
+    //     borderRadius: BorderRadius.circular(30),
+    //     color: Colors.white,
+    //     boxShadow: const [
+    //       BoxShadow(
+    //         offset: Offset(0, 0),
+    //         blurRadius: 6,
+    //         color: Colors.black38,
+    //       ),
+    //     ],
+    //   ),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       // Container(
+    //       //   // height: 200,
+    //       //   // width: 200,
+    //       //   child: Image.asset(
+    //       //     image,
+    //       //     fit: BoxFit.cover,
+    //       //     width: 200,
+    //       //     height: 200,
+    //       //     // scale: ,
+
+    //       //   ),
+    //       // ),
+    //       Expanded(
+    //         child: ClipRRect(
+    //           borderRadius: BorderRadius.circular(30),
+    //           child: Image.asset(
+    //             image,
+    //             fit: BoxFit.cover,
+    //           ),
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: const EdgeInsets.symmetric(horizontal: 16),
+    //         child: Text(
+    //           title,
+    //           style: TextStyle(
+    //             color: Colors.black,
+    //             fontWeight: FontWeight.bold,
+    //             fontSize: 20,
+    //           ),
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: const EdgeInsets.symmetric(horizontal: 16),
+    //         child: Text(
+    //           '$value People',
+    //           style: TextStyle(
+    //             color: Colors.black,
+    //             fontSize: 14,
+    //           ),
+    //         ),
+    //       ),
+    //       SizedBox(height: 16),
+    //     ],
+    //   ),
+    // );
   }
 }
